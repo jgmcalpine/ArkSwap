87a5652152317e840d1c11ac57b6909e0c38a5b7
Jon <j.g.mcalpine@gmail.com>
2025-11-30 15:35:28 -0600

    Export prisma model types

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,8 +1,9 @@
 export { PrismaClient } from '@prisma/client';
-export type {
-  AspDefinition,
-  ScannedBlock,
-  ArkRound,
-  Prisma,
-} from '@prisma/client';
+import type { Prisma } from '@prisma/client';
+
+// Export model types using Prisma's generated types
+export type AspDefinition = Prisma.AspDefinitionGetPayload<{}>;
+export type ScannedBlock = Prisma.ScannedBlockGetPayload<{}>;
+export type ArkRound = Prisma.ArkRoundGetPayload<{}>;
+export type { Prisma };
 
